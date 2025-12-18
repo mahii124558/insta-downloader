0ff784099a16638f1cdcbfa2a7f62668c7a4548c
mahii124558 <mahiitech3@gmail.com>
2025-12-18 21:37:22 +0530

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,14 @@
+body {
+  font-family: Arial;
+  text-align: center;
+  background: #f2f2f2;
+}
+input {
+  width: 80%;
+  padding: 10px;
+}
+button {
+  padding: 10px 20px;
+  margin-top: 10px;
+}
 
